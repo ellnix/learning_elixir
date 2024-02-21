0840af2c9464d07f9dc65e31073d12782d971f6a
ellnix <103502144+ellnix@users.noreply.github.com>
2024-02-21 10:02:40 +0100

    Document code

--- lib/learning_elixir.ex
+++ lib/learning_elixir.ex
@@ -29,15 +29,20 @@ defmodule MathFunctions do
   def math_api_call(url), do: {:ok, 5}
 end
 
+IO.puts "Recursive function"
 IO.puts MathFunctions.factorial(5)
+
+IO.puts "Function with default value"
 IO.puts MathFunctions.double(10)
 IO.puts MathFunctions.double
 
+IO.puts "Using a with statement"
 with {:ok, n} = MathFunctions.math_api_call("hello")
 do
   IO.puts n
 end
 
+IO.puts "Accessing and updating a nested Map"
 parameters = %{
   "_csrf_token" => "UR99GwILHTtzbSBUYRwmBVpdeDY/AAAA3K70jEiO9UhgPVwh+d3WYw==",
   "_utf8" => "✓", 
@@ -50,8 +55,10 @@ IO.puts password
 parameters = %{ parameters | "_csrf_token" => nil }
 IO.inspect(parameters)
 
+IO.puts "Accessing a list"
 my_list = [1, 2, 3, 4]
 [first | rest] = my_list
 IO.puts first
 [_, second | rest] = my_list
 IO.puts second
+
